511eefd3a48a9d2a69bb18acb39abedddbf78378
Ram <104311709+RamGemes@users.noreply.github.com>
2024-12-26 22:13:07 -0500

    V:0.1.1 port

--- src/main/java/net/ramixin/mixson/events/AdvancedCreationEvent.java
+++ src/main/java/net/ramixin/mixson/events/AdvancedCreationEvent.java
@@ -3,12 +3,13 @@ package net.ramixin.mixson.events;
 import com.google.gson.JsonElement;
 import net.minecraft.resources.ResourceLocation;
 import net.ramixin.mixson.BuiltResourceReference;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 
 @FunctionalInterface
 public interface AdvancedCreationEvent extends MixsonEventTypes.Creation {
 
-    JsonElement run(HashMap<ResourceLocation, BuiltResourceReference> references);
+    @Nullable JsonElement run(HashMap<ResourceLocation, BuiltResourceReference> references);
 
 }
--- src/main/java/net/ramixin/mixson/events/AdvancedModificationEvent.java
+++ src/main/java/net/ramixin/mixson/events/AdvancedModificationEvent.java
@@ -3,12 +3,13 @@ package net.ramixin.mixson.events;
 import com.google.gson.JsonElement;
 import net.minecraft.resources.ResourceLocation;
 import net.ramixin.mixson.BuiltResourceReference;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 
 @FunctionalInterface
 public interface AdvancedModificationEvent extends MixsonEventTypes.Modification {
 
-    JsonElement run(JsonElement elem, HashMap<ResourceLocation, BuiltResourceReference> references);
+    @NotNull JsonElement run(JsonElement elem, HashMap<ResourceLocation, BuiltResourceReference> references);
 
 }
